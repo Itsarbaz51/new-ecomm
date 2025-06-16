8ce0328fc369f44f1d1bbd46ae323c33f14eb70d
PawanVerma1 <Pm9521263317@gmail.com>
2025-06-16 10:32:28 +0530

    done

--- resources/views/layouts/headerFooter/header.blade.php
+++ resources/views/layouts/headerFooter/header.blade.php
@@ -383,8 +383,8 @@
         <div id="mobile-nav" class="hidden h-screen p-6 space-y-2.5">
             {{-- Place your nav and user/account section here as-is --}}
             <nav class="main-nav flex flex-col space-y-2">
-                <a href="{{ route('home.index') }}" class="bg-red-100 p-2 hover:bg-red-200 rounded">Home</a>
-                <div class="dropdown bg-red-100 p-2 hover:bg-red-200 rounded">
+                <a href="{{ route('home.index') }}" class="p-2 hover:bg-red-100 rounded">Home</a>
+                <div class="dropdown p-2 hover:bg-red-100 rounded">
                     <a href="{{ route('shop.index') }}">Shop ▾</a>
                     <ul class="dropdown-menu pl-4">
                         @foreach ($categories as $category)
@@ -396,9 +396,9 @@
                         @endforeach
                     </ul>
                 </div>
-                <a href="{{ route('home.faqs') }}" class="bg-red-100 p-2 hover:bg-red-200 rounded">FAQs</a>
-                <a href="{{ route('home.about') }}" class="bg-red-100 p-2 hover:bg-red-200 rounded">About Us</a>
-                <a href="{{ route('home.coupons') }}" class="bg-red-100 p-2 hover:bg-red-200 rounded">Offers</a>
+                <a href="{{ route('home.faqs') }}" class="p-2 hover:bg-red-100 rounded">FAQs</a>
+                <a href="{{ route('home.about') }}" class="p-2 hover:bg-red-100 rounded">About Us</a>
+                <a href="{{ route('home.coupons') }}" class="p-2 hover:bg-red-100 rounded">Offers</a>
             </nav>
             <div class="actions flex">
                 <!-- <div><a href="#" class="me-3"><i class="bi bi-search"></i></a><a href="cart.html"><i class="bi bi-cart"></i>0</a></div> -->
